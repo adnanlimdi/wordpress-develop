496763f3d93cf5511195dc45dd56a4f2f0d88686
adnanlimdi <adnan.limdiwala@gmail.com>
2024-02-22 13:17:01 +0530

    changes in theme

--- src/wp-content/themes/twentyseventeen/single.php
+++ src/wp-content/themes/twentyseventeen/single.php
@@ -17,12 +17,14 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 			<?php
+
 			// Start the Loop.
 			while ( have_posts() ) :
 				the_post();
 
 				get_template_part( 'template-parts/post/content', get_post_format() );
 
+				
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
